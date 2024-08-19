c11394755b3872bf42260c3ad0b01d121b1ad892
sLdagama <samuelluck196@gmail.com>
2024-08-19 15:06:01 -0300

    mudança no footer (fixado na parte de baixo)

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>sldagama</title>
     <script src="https://cdn.tailwindcss.com"></script> 
-    
 </head>
-<body>
+<body class="flex flex-col min-h-screen">
     <header class="bg-gray-800 text-white p-6 flex justify-between items-center">
         <div class="text-2xl font-bold">
             <a href="{{ route('home') }}">sLdagama</a>
@@ -18,9 +17,11 @@
         </a>
     </header>
 
-    @yield('content')
+    <main class="flex-grow">
+        @yield('content')
+    </main>
 
-    <footer class="bg-gray-800 text-white text-center p-4 mt-4">
+    <footer class="bg-gray-800 text-white text-center p-4">
         &copy; 2024 direitos reservados para sLdagama
     </footer>
     
