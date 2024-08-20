cb65dda30082124c5c3415d89300f6a5e6ad8c8b
sLdagama <samuelluck196@gmail.com>
2024-08-20 13:45:28 -0300

    Ajustando seeds e factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,13 +15,19 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    protected $model = \App\Models\User::class;
+     public function definition()
     {
+        $validCep = [
+            '01001-000',
+            '20010-000',
+            '30110-000',
+            '40010-000',
+        ];
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'name' => $this->faker->name(),
+            'cep' => $this->faker->randomElement($validCep),
+            'phone' => $this->faker->phoneNumber(),
             'remember_token' => Str::random(10),
         ];
     }
